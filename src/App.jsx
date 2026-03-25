b04ffe998c11eb5ee114149504e9e662be2db11a
sumon550 <entertainmentcharacter54321@gmail.com>
2026-03-25 19:06:53 +0600

    Initialize personal-app3 project

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+
+const navigationData = [
+  {
+    id: 1,
+    name: "Home",
+    path: "/",
+  },
+  {
+    id: 2,
+    name: "About",
+    path: "/about",
+  },
+  {
+    id: 3,
+    name: "Services",
+    path: "/services",
+  },
+  {
+    id: 4,
+    name: "Blog",
+    path: "/blog",
+  },
+  {
+    id: 5,
+    name: "Contact",
+    path: "/contact",
+  },
+];
+
+function App() {
+  return <></>;
+}
+
+export default App;
